bccbf3ab7d99c2b87b3381c3df28b98c33114768
hitman46923 <al46923@outlook.com>
2024-01-22 12:38:33 +0300

    feat: complete task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+
+const fs = require ("fs");
+ const path = require("path");
+
+ const filePath = path.join(__dirname , "text.txt" )
+
+const readStream = fs.createReadStream(filePath, "utf-8");
+
+readStream.on('data',(chunk) => {
+ console.log(chunk);
+});
+
+readStream.on('error', (error) => {
+    console.log('error' , error.message) 
+}) 
